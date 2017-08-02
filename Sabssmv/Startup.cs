0b2232b13d90f3cad78484bf6e6348e0445e0579
Gideon Nimako <gideonnimako@Gideons-MacBook-Pro.local>
2017-08-02 10:09:42 +0200

    First commit

---
+++ Sabssmv/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sabssmv.Startup))]
+namespace Sabssmv
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
